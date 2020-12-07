548f1479a99bb222eebdbf6e3bfd9d149c030d31
Ember Parr <ember.devops@gmail.com>
2020-12-07 13:58:14 -0600

    added postManager cs file

---
+++ TabloidCLI/UserInterfaceManagers/PostManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    class PostManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private PostRepository _postRepository;
+        private string _connectionString;
+
+        public PostManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _postRepository = new PostRepository(connectionString);
+            _connectionString = connectionString;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Post Menu");
+            Console.WriteLine(" 1) New Post");
+            Console.WriteLine(" 2) Nothing here yet");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Add();
+                    return this;
+                case "2":
+                    Console.WriteLine("nothing yet....");
+                    return this;
+                default:
+                    Console.WriteLine("Invlid Selection");
+                    return this;
+            }
+        }
+
+        private void Add()
+        {
+            Console.WriteLine("New Post");
+            Post post = new Post();
+
+            Console.Write("Title: ");
+            post.Title = Console.ReadLine();
+
+            Console.Write("URL: ");
+            post.Url = Console.ReadLine();
+
+            Console.Write("Publish Date: ");
+            post.PublishDateTime = DateTime.Parse(Console.ReadLine());
+
+            // author and blog go here
+
+            _postRepository.Insert(post);
+        }
+
+    }
+}
