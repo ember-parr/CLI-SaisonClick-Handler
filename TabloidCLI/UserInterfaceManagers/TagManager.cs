6945947b643b348b2640f90e1fa30052e7d0ccb2
Joe <overtonje@gmail.com>
2020-12-08 15:21:01 -0600

    finished edit and remove

--- TabloidCLI/UserInterfaceManagers/TagManager.cs
+++ TabloidCLI/UserInterfaceManagers/TagManager.cs
@@ -19,7 +19,7 @@ namespace TabloidCLI.UserInterfaceManagers
         public IUserInterfaceManager Execute()
         {
             Console.WriteLine("------------------------------");
-            Console.WriteLine("|         Tag Menu          |");
+            Console.WriteLine("|         Tag Menu        1   |");
             Console.WriteLine("------------------------------");
             Console.WriteLine(" 1) List Tags");
             Console.WriteLine(" 2) Add Tag");
@@ -76,14 +76,61 @@ namespace TabloidCLI.UserInterfaceManagers
 
             _tagRepository.Insert(tag);
         }
-            private void Edit()
+        private void Edit()
         {
-            throw new NotImplementedException();
+            Tag tagToEdit = Choose("Which tag would you lie to edit");
+            if (tagToEdit == null)
+            {
+                return;
+            }
+            Console.WriteLine();
+            Console.WriteLine("New name(blank to leave unchanged): ");
+            string name = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                tagToEdit.Name = name;
+            }
+            _tagRepository.Update(tagToEdit);
         }
 
         private void Remove()
         {
-            throw new NotImplementedException();
+            Tag tagToDelete = Choose("Which tag would you like to remove?");
+            if (tagToDelete != null)
+            {
+                _tagRepository.Delete(tagToDelete.Id);
+            }
+        }
+        public Tag Choose(string prompt = null)
+        {
+            if (prompt == null)
+            {
+                prompt = "Please choose a Tag:";
+            }
+
+            Console.WriteLine(prompt);
+
+            List<Tag> tags = _tagRepository.GetAll();
+
+            for (int i = 0; i < tags.Count; i++)
+            {
+                Tag tag = tags[i];
+                Console.WriteLine($"{i + 1} {tag.Name}");
+            }
+            Console.Write("> ");
+            string input = Console.ReadLine();
+            try
+            {
+                int choice = int.Parse(input);
+                return tags[choice - 1];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid Selection");
+                return null;
+            }
+
+
         }
     }
 }
