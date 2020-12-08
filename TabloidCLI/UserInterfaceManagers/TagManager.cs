770ad2513518eca38cc7f4372b96bd9b2d004af6
Joe <overtonje@gmail.com>
2020-12-08 13:39:26 -0600

    addtag complete

--- TabloidCLI/UserInterfaceManagers/TagManager.cs
+++ TabloidCLI/UserInterfaceManagers/TagManager.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using TabloidCLI.Models;
 
 namespace TabloidCLI.UserInterfaceManagers
 {
     public class TagManager : IUserInterfaceManager
     {
         private readonly IUserInterfaceManager _parentUI;
-
+        private TagRepository _tagRepository;
+        private string _connectionString;
         public TagManager(IUserInterfaceManager parentUI, string connectionString)
         {
             _parentUI = parentUI;
+            _tagRepository = new TagRepository(connectionString);
+            _connectionString = connectionString;
         }
 
         public IUserInterfaceManager Execute()
@@ -51,10 +55,15 @@ namespace TabloidCLI.UserInterfaceManagers
 
         private void Add()
         {
-            throw new NotImplementedException();
-        }
+            Console.WriteLine("New Tag");
+            Tag tag = new Tag();
 
-        private void Edit()
+            Console.Write("Tag Name: ");
+            tag.Name = Console.ReadLine();
+
+            _tagRepository.Insert(tag);
+        }
+            private void Edit()
         {
             throw new NotImplementedException();
         }
