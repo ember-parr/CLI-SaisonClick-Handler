eec7f7febbef980b325b76d39c5a3a5420790645
Terra Roush <terra.roush@gmail.com>
2020-12-07 19:23:56 -0600

    working journal list query in journal repository; proper writeline in journalManager; updated writeline in main menu manager and updated case statement accordingly.

---
+++ TabloidCLI/UserInterfaceManagers/JournalManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    public class JournalManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private JournalRepository _journalRepository;
+        private string _connectionString;
+
+        public JournalManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _journalRepository = new JournalRepository(connectionString);
+            _connectionString = connectionString;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Journal Menu");
+            Console.WriteLine(" 1) List Entries");
+            Console.WriteLine(" 2) Entry Details");
+            Console.WriteLine(" 3) Add Entry");
+            Console.WriteLine(" 4) Edit Entry");
+            Console.WriteLine(" 5) Remove Entry");
+            Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    List();
+                    return this;
+                //case "2":
+                //    Journal journal = Choose();
+                //    if (journal == null)
+                //    {
+                //        return this;
+                //    }
+                //    else
+                //    {
+                //        return new JournalDetailManager(this, _connectionString, journal.Id);
+                //    }
+                //case "3":
+                //    Add();
+                //    return this;
+                //case "4":
+                //    Edit();
+                //    return this;
+                //case "5":
+                //    Remove();
+                //    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        private void List()
+        {
+            List<Journal> journals = _journalRepository.GetAll();
+            foreach (Journal journal in journals)
+            {
+                Console.WriteLine(journal.Title);
+            }
+        }
+    }
+}
